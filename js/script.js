870b7bb8a1a070102916ee6abd7a6086e2bc0f63
andreyrise <andrey.rise@gmail.com>
2021-12-09 13:00:21 +0200

    add new blocks

--- js/script.js
+++ js/script.js
@@ -26,3 +26,32 @@ $(document).ready(function () {
       ],
     });
   });
+
+  $(document).ready(function () {
+    $('.slider-brands').slick({
+      arrows: true,
+      dots: false,
+      adaptiveHeight: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      speed: 700,
+      easing: 'linear',
+      infinite: true,
+      initialSlide: 1,
+      autoplay: false,
+      autoplaySpeed: 1500,
+      pauseOnFocus: true,
+      pauseOnHover: true,
+      pauseOnDotsHover: true,
+      rows: 1,
+      slidesPerRows: 1,
+      responsive: [
+        {
+          breakpoint: 320,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ],
+    });
+  });
