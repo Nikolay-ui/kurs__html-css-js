8be996803fc66eda13cf296c4182e4ee82d85ace
andreyrise <andrey.rise@gmail.com>
2021-12-10 15:50:30 +0200

    add new block and styles file

--- js/script.js
+++ js/script.js
@@ -65,3 +65,42 @@ $(document).ready(function () {
       ],
     });
   });
+  $(document).ready(function () {
+    $('.speakers-slider').slick({
+      arrows: true,
+      dots: false,
+      adaptiveHeight: true,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      speed: 700,
+      easing: 'linear',
+      infinite: true,
+      initialSlide: 1,
+      autoplay: false,
+      autoplaySpeed: 1500,
+      pauseOnFocus: true,
+      pauseOnHover: true,
+      pauseOnDotsHover: true,
+      rows: 1,
+      slidesPerRows: 1,
+      responsive: [
+        {
+          breakpoint: 1068,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2
+          }
+        },{
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ],
+    });
+  });
