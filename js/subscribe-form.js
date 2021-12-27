0e57386bd0ea6ca61b7dd56494825c67904b3a1f
andreyrise <andrey.rise@gmail.com>
2021-12-27 11:42:00 +0200

    correct forms code

--- js/subscribe-form.js
+++ js/subscribe-form.js
@@ -2,8 +2,8 @@ const form = document.getElementById("my-form");
 
 async function handleSubmit(event) {
     event.preventDefault();
-    var status = document.getElementById("my-form-status");
-    var data = new FormData(event.target);
+    let status = document.getElementById("my-form-status");
+    let data = new FormData(event.target);
     fetch(event.target.action, {
         method: form.method,
         body: data,
@@ -11,7 +11,7 @@ async function handleSubmit(event) {
             'Accept': 'application/json'
         }
     }).then(response => {
-        status.innerHTML = "Thanks for your submission!";
+        status.innerHTML = "Thanks for your subscribed!";
         form.reset()
     }).catch(error => {
         status.innerHTML = "Oops! There was a problem submitting your form"
