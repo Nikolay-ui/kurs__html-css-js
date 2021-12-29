f7fbb5cb87bdf56a3303415bf8db77e9b99f9f11
YevhenM228 <melnyk1509@gmail.com>
2021-12-29 14:51:13 +0000

    Update js

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -2,16 +2,16 @@
     const iconBurger = document.querySelector(".header_navigation-burger");
     const navigationsBody = document.querySelector(".header_navigation-wrap");
     iconBurger.addEventListener("click", function (e) {
-      document.body.classList.toggle("lock");
+      document.body.classList.toggle("_lock");
       iconBurger.classList.toggle("active");
       navigationsBody.classList.toggle("active");
     });
   
-    const navLinks = document.querySelectorAll(".navigation__link");
+    const navLinks = document.querySelectorAll(".header_navigation-wrap-pages");
     navLinks.forEach((navLink) => {
       navLink.addEventListener("click", (_) => {
             if (iconBurger.classList.contains("active")) {
-                document.body.classList.remove("lock");
+                document.body.classList.remove("_lock");
                 iconBurger.classList.remove("active");
                 navigationsBody.classList.remove("active");
             }
