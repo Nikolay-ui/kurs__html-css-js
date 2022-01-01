f046c5d1ed752c2ceefd1835b5d4eaa948c009d6
nikolay <mazurov.nikolay000@gmail.com>
2022-01-01 21:01:06 +0200

    js carousel update

--- js/main-headphones-carousel.js
+++ js/main-headphones-carousel.js
@@ -1,12 +1,34 @@
-(function() {
+(async function() {
 
-    const slides = [
-        '<div><img src="img/headphones/jbl-334wn.png"" alt="JBL 334WN"></div>',
-        '<div><img src="img/headphones/airpulse-bb34.png" alt="AirPulse BB34"></div>',
-        '<div><img src="img/headphones/bang-and-olufsen-f112.png" alt="Bang & Olufsen F112"></div>',
-        '<div><img src="img/headphones/airpulse-rs45.png" alt="AirPulse RS45"></div>',
-        '<div><img src="img/headphones/sony-tt56.png" alt="Sony TT56"></div>'
-    ]
+    const slides = []
+
+    const response = await fetch('main-headphones.json');
+    const products = await response.json();
+
+    function renderProducts(product) {
+    const productsContainer = document.querySelector('.main-headphones-carousel__slide');
+        let html = '';
+        for (const product of products) {
+            const productHTML = 
+            html = `
+            <article class="headphones-card">
+                <div class="content">
+                    <a class="a" href="#">
+                        <img class="headphones-image" src="${product.image}" alt="${product.name}">
+                        <h4 class="h4-name">${product.name}</h4>
+                    </a>
+                    <h4 class="h4-price">${product.price}</h4>
+                    <button class="button-add-cart">
+                        Add to Cart
+                    </button>
+                </div>
+            </article>`;
+            slides.push(html);
+        }
+        productsContainer.innerHTML = html;
+    }
+
+    renderProducts(products);
 
     let currentSlide = 0;
 
